29382e2c3d16871a5015cc77093770ba1b88dad6
BSick7 <brad.sickles@gmail.com>
2014-04-25 15:38:13 -0400

    Added day of week.

---
+++ TimesheetApp/TimesheetApp/Resources/DayOfWeekConverter.ts
@@ -0,0 +1,13 @@
+﻿class DayOfWeekConverter implements Fayde.Data.IValueConverter {
+    Convert(value: any, targetType: IType, parameter: any, culture: any): any {
+        var dt = <DateTime>value;
+        if (!dt || !(dt instanceof DateTime))
+            return "-";
+        return DayOfWeek[dt.DayOfWeek];
+    }
+    ConvertBack(value: any, targetType: IType, parameter: any, culture: any): any {
+        throw new Error("NotImplemented");
+    }
+}
+Fayde.RegisterTypeInterfaces(DayOfWeekConverter, Fayde.Data.IValueConverter_);
+export = DayOfWeekConverter;
