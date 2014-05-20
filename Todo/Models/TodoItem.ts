53ea290f199b17eef51c2d4222625956f9db6821
BSick7 <brad.sickles@gmail.com>
2014-05-20 09:14:46 -0400

    Created basic todo demo.

---
+++ Todo/Models/TodoItem.ts
@@ -0,0 +1,10 @@
+﻿class TodoItem extends Fayde.MVVM.ObservableObject {
+    Text: string = "";
+    IsComplete: boolean = false;
+
+    ToggleComplete(e: Fayde.IEventBindingArgs<Fayde.Input.MouseButtonEventArgs>) {
+        this.IsComplete = !this.IsComplete;
+    }
+}
+Fayde.MVVM.NotifyProperties(TodoItem, ["Text", "IsComplete"]);
+export = TodoItem; 
---
+++ Todo/ViewModels/MainViewModel.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../lib/Fayde/Fayde.d.ts" />
+
+import TodoItem = require("../Models/TodoItem");
+
+class MainViewModel extends Fayde.MVVM.ViewModelBase {
+    Items = new Fayde.Collections.DeepObservableCollection<TodoItem>();
+    ActiveText: string = "";
+
+    get NumItemsLeft(): number {
+        var count = 0;
+        var enumerator = this.Items.GetEnumerator();
+        while (enumerator.MoveNext()) {
+            if (!enumerator.Current.IsComplete)
+                count++;
+        }
+        return count;
+    }
+
+    constructor() {
+        super();
+        this.Items.ItemPropertyChanged.Subscribe(this._OnItemPropertyChanged, this);
+    }
+
+    private _OnItemPropertyChanged(sender: any, e: Fayde.Collections.ItemPropertyChangedEventArgs<TodoItem>) {
+        this.OnPropertyChanged("NumItemsLeft");
+    }
+
+    AddTodo(e: Fayde.IEventBindingArgs<Fayde.Input.KeyEventArgs>) {
+        if (e.args.Key !== Fayde.Input.Key.Enter)
+            return;
+        if (!this.ActiveText)
+            return;
+        var item = new TodoItem();
+        item.Text = this.ActiveText;
+        this.Items.Add(item);
+        this.ActiveText = "";
+        this.OnPropertyChanged("NumItemsLeft");
+    }
+}
+Fayde.MVVM.NotifyProperties(MainViewModel, ["ActiveText"]);
+export = MainViewModel; 
