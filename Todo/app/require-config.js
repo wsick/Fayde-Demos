42cf9ff28bad6558e9144f41dcc7627049ed3d02
BSick7 <brad.sickles@gmail.com>
2014-11-07 13:38:48 -0500

    Upgraded fayde runtime for todo app.

--- Todo/app/require-config.js
+++ Todo/app/require-config.js
@@ -1,21 +1,10 @@
 var require = {
     baseUrl: "./",
     paths: {
-        "text": "lib/requirejs-text/text",
-        "Fayde": "lib/Fayde/Fayde"
+        "text": "lib/requirejs-text/text"
     },
-    deps: ["text", "Fayde"],
-    callback: function (text, Fayde) {
-        Fayde.LoadConfigJson(function (config, err) {
-            if (err)
-                console.warn('Could not load fayde configuration file.', err);
-            Fayde.Run();
-        });
-    },
-    shim: {
-        "Fayde": {
-            exports: "Fayde",
-            deps: ["text"]
-        }
+    deps: ["text", "text!fayde.json"],
+    callback: function (text, config) {
+        Fayde.Bootstrap();
     }
 };
