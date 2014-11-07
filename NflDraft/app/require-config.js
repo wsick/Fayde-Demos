cc82d1eb6db33a8bf1b96e8b17d2d8e3630760bd
BSick7 <brad.sickles@gmail.com>
2014-11-07 13:41:07 -0500

    Upgraded fayde runtime for NflDraft.

--- NflDraft/app/require-config.js
+++ NflDraft/app/require-config.js
@@ -3,12 +3,8 @@ var require = {
     paths: {
         "text": "lib/requirejs-text/text"
     },
-    deps: ["text"],
-    callback: function (text) {
-        Fayde.LoadConfigJson(function (config, err) {
-            if (err)
-                console.warn('Could not load fayde configuration file.', err);
-            Fayde.Run();
-        });
+    deps: ["text", "text!fayde.json"],
+    callback: function (text, config) {
+        Fayde.Bootstrap();
     }
 };
