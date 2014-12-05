d64b268ca8358abeb059b074db94038908888d6b
BSick7 <brad.sickles@gmail.com>
2014-12-05 12:21:45 -0500

    Fixed demo.

--- ControlsShowcase/app/ViewModels/MainViewModel.ts
+++ ControlsShowcase/app/ViewModels/MainViewModel.ts
@@ -6,19 +6,20 @@ class MainViewModel extends Fayde.MVVM.ViewModelBase {
             {
                 Name: "Level 2 - 1",
                 Children: [
-                    { Name: "Level 3 - 1" },
-                    { Name: "Level 3 - 2" }
+                    {Name: "Level 3 - 1"},
+                    {Name: "Level 3 - 2"}
                 ]
             }
         ]
     }];
-    constructor() {
+
+    constructor () {
         super();
-        this.States.push({ Name: "Florida" });
-        this.States.push({ Name: "Georgia" });
-        this.States.push({ Name: "South Carolina" });
-        this.States.push({ Name: "Alabama" });
-        this.States.push({ Name: "Tennessee" });
+        this.States.push({Name: "Florida"});
+        this.States.push({Name: "Georgia"});
+        this.States.push({Name: "South Carolina"});
+        this.States.push({Name: "Alabama"});
+        this.States.push({Name: "Tennessee"});
     }
 }
 export = MainViewModel;
